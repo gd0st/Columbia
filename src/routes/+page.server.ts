34267d3cae0a467833655076444cdaef544e4771
Yannick <BrokeBoat@protonmail.com>
2022-09-11 15:21:45 -0500

    adding recaptcha, failing to load rn

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,7 +1,9 @@
 import { profile_card } from "../../profile_config.js"
+import { recaptcha_site } from "../../site_keys"
 export async function load() {
 	return {
 		profile: profile_card,
 		socialLinks: profile_card.links,
+		recaptcha_site: recaptcha_site
 	}
 }
