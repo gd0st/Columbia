b1b1ce751bf976ba16c3717d02578e4edfdf36cf
Yannick <yannick.dorn@gmail.com>
2022-10-23 18:22:28 -0500

    working on the cards. Color system still requires more configuration

---
+++ src/components/Skills/Skill.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import type { SkillInterface } from '../../lib/skills';
+	import Icon from '../Icon.svelte';
+	export let skill: SkillInterface;
+</script>
+
+<a href=".">
+	<div class="{skill.color} rounded border-black border-2 m-5 px-3 py-5">
+		<Icon faSource={skill.icon} />
+	</div>
+</a>
+
+<style>
+
+	.golang {
+		background-color: #29beb0;
+	}
+	.javascript {
+		background-color: #f0db4f;
+	}
+</style>
--- src/components/Skills/Skills.svelte
+++ src/components/Skills/Skills.svelte
@@ -1,37 +1,37 @@
 <script lang="ts">
-	import Skill from "./Skill.svelte";
-import type { Skill } from "./skills"
-	import { skills } from "./skills"
-	import { getCategories, filterForCategory } from "./skills"
+	import { xlink_attr } from "svelte/internal";
+	import type { SkillInterface } from "../../lib/skills"
+	import  Skill  from "./Skill.svelte"
+	export let skills: SkillInterface[]
 
-	let skillCategories: string[] = skills.map(
-		skill => skill.category
-	)
-	skillCategories = [...new Set(skillCategories)]
-	let skillsMapped = skillCategories.forEach(
-		cat => skills.filter(
-			skill => skill.category == cat
-		)
-	)
+	// let skillCategories: string[] = skills.map(
+	// 	skill => skill.category
+	// )
+	// skillCategories = [...new Set(skillCategories)]
+	// let skillsMapped = skillCategories.forEach(
+	// 	cat => skills.filter(
+	// 		skill => skill.category == cat
+	// 	)
+	// )
+
+	let colors = [
+		"blue",
+		"teal",
+		"violet",
+		"fuchisa"
+	]
+
+	colors = colors.map(
+		color => `bg-${color}-500`
+	);
+
+
 </script>
 
 
 <style></style>
 
+{#each skills as skill}
+		<Skill {skill}/>
+{/each}
 
-<div class="grid grid-cols-3">
-	{#each getCategories(skills) as category}
-	<div class="grid grid-cols-3 mx-auto">
-		<div>
-		<h1 class="text-center">{category}</h1>
-		{#each filterForCategory(skills, category) as skill}
-				<p>{skill.name}</p>
-				<p>{skill.skillLevel}</p> 
-				{#if skill.github}
-				<p>{skill.github}</p>
-				{/if}
-		{/each}
-		</div>
-	</div>
-	{/each}
-</div>
