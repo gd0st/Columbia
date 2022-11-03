9c8a8870e1f7886d5371115d74351ff2dddffa67
Yannick <yannick.dorn@gmail.com>
2022-11-02 21:34:25 -0500

    finally got the social media navigation bar configured :)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -22,17 +22,33 @@
 
 
 <div class="flex flex-col items-center text-[#e0e1dd] w-screen md:w-1/2 lg:w-1/4 md:m-auto">
-	<div class="p-3 border rounded-t-md bg-[#1B263B] text-[#e0e1dd]">
+	<div class="p-3 border rounded-tl-md bg-[#1B263B] text-[#e0e1dd]">
 		<h1 class="text-4xl font-pressstart  text-center">Yannick Dorn</h1> 
 	</div>
 	{#if socialMedia.length > 0}
-		<div class="flex flex-row">
-			{#each socialMedia as media}
-				<div class="px-12 py-4 hover:bg-[#e0e1dd] hover:text-[#1b263b]"><Icon faSource={media.icon}/></div>
+		<!-- <div class="flex flex-row box-border w-full"> -->
+			<nav>
+				<ul class="flex flex-row box-border w-full">
+				{#each socialMedia as media}
+
+					<li class="w-1/3 border-x border-dashed">
+						<a href="{media.hyperlink}">
+							<div class="p-5 text-center hover:bg-[#e0e1dd] hover:text-[#1b263b] ">
+								<Icon faSource={media.icon}/>
+							</div>
+						</a>
+					</li>
+			<nav>
+			<!-- <a href="{media.hyperlink}">
+					<div class="text-center border-x border-dashed p-5 hover:bg-[#e0e1dd] hover:text-[#1b263b] w-1/3">
+							<Icon faSource={media.icon}/>
+					</div>
+			</a> -->
 			{/each}
-		</div>
+			</ul>
+		<!-- </div> -->
 	{/if}
-	<div class="square-color px-2 border rounded-b-md">
+	<div class="square-color px-2 border rounded-br-md">
 		<div class="grid grid-cols-1 font-inconsolata text-xl">
 			{#each content as line}
 			<p class="p-1">{line}</p>
