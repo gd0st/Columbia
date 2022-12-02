f65c552fab83eb16841a2d8cd1ee604331231343
Yannick Dorn <yannick.dorn@gmail.com>
2022-12-01 22:55:57 -0500

    added simple info changer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -28,14 +28,28 @@
 		content: 'Yannick Dorn',
 		font: 'pressstart'
 	}
+
+	async function getIndexParagraphs() {
+		const request = await fetch('/', {
+			method: 'GET',
+			headers: {
+				'content-type': 'application/json',
+				'cache-control': 'public, max-age=3600'
+			}
+		})
+
+		const data = await request.json()
+		console.log(data)
+
+		return data.text_data
+
+	}
+
 </script>
 
 <svelte:head>
 	<title>Yannick Dorn</title>
 </svelte:head>
-
-
-
 	<!-- <div class="p-3 border rounded-tl-md  text-[#e0e1dd] border-b-0"> -->
 		<!-- <h1 class="text-4xl font-pressstart  text-center">Yannick Dorn</h1> -->
 		<Header headerMeta={headerMeta}/>
@@ -71,13 +85,17 @@
 	</div>
 	<div class="square-color px-2 border rounded-br-md border-t-0">
 		<div class="grid grid-cols-1 font-inconsolata text-xl">
-			{#each content as line}
-				<p class="p-1">{line}</p>
-				<!-- <p class="pb-1">Hi,</p>
-			<p class="p-1">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est soluta eum rem labore quis excepturi, ducimus nesciunt harum totam veniam libero saepe repudiandae consequuntur minus sint sequi! Perspiciatis, at. A!</p>
-			<p class="p-1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, architecto repudiandae. Quasi voluptate laboriosam placeat voluptatum error, perferendis inventore aperiam beatae fugiat aspernatur. Dicta rerum ut mollitia nisi beatae totam!</p>
-			<p class="p-1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt explicabo sequi dignissimos! Explicabo deleniti rerum vitae pariatur soluta voluptatibus. Reprehenderit a voluptates doloremque voluptate aspernatur animi, deleniti nemo eos. Reiciendis.</p> -->
-			{/each}
+			{#await getIndexParagraphs()}
+
+			<p>Loading Content...</p>
+				
+			{:then text_data} 
+				{#each text_data as line}
+					<p class="p-1">{line}</p>
+					
+				{/each}
+				
+			{/await}
 		</div>
 		<ul class="flex flex-row box-border w-full">
 			{#each socialMedia as media}
